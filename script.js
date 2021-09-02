af0c0064f965c39d5c8dfe534f8e77bd8ddead76
saidulislam <mislam171222@bscse.uiu.ac.bd>
2021-09-02 23:38:47 +0600

    service page complete

--- script.js
+++ script.js
@@ -46,3 +46,21 @@ $(function() {
   AOS.init({
     duration: 1200,
   })
+
+// service slider
+
+$(document).ready(function () {
+  var getActiveSlideIndex = function () {
+    return $("#productSlider .active").index("#productSlider .carousel-item");
+  };
+  var slider = function (IndexNumber) {
+    $(".slider .image")
+      .removeClass("active")
+      .eq(IndexNumber)
+      .addClass("active");
+  };
+  slider(getActiveSlideIndex());
+  $("#productSlider").on("slid.bs.carousel", function () {
+    slider(getActiveSlideIndex());
+  });
+});
